4bd6ef33d826aaaebb9f33bf7730952860eb7cd4
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-01-19 11:31:16 +0100

    SONAR-8681 delete perm,groups&components in api/organizations/delete

--- sonar-db/src/main/java/org/sonar/db/component/ComponentMapper.java
+++ sonar-db/src/main/java/org/sonar/db/component/ComponentMapper.java
@@ -77,6 +77,8 @@ public interface ComponentMapper {
    */
   List<ComponentDto> selectProjects();
 
+  List<ComponentDto> selectAllRootsByOrganization(@Param("organizationUuid") String organizationUuid);
+
   /**
    * Return all descendant modules (including itself) from a given component uuid and scope
    */
